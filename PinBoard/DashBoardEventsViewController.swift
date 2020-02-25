b7982dd45db14d4f63f975ef933aa6313cb37156
Alexis <s026134@students.lmsd.org>
2020-02-24 21:27:14 -0500

    Modified story board for create event and dashboard events view controllers

---
+++ PinBoard/DashBoardEventsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DashBoardEventsViewController.swift
+//  PinBoard
+//
+//  Created by Alexis Duong (student LM) on 2/24/20.
+//  Copyright © 2020 Alexis Duong (student LM). All rights reserved.
+//
+
+import UIKit
+
+class DashBoardEventsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
