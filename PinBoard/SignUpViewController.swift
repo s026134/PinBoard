2ed869366d233072dbbdeb864c78f53d4046efd8
Justin Minerva <s013836@students.lmsd.org>
2020-02-26 08:52:37 -0500

    sign up and preference view controllers sign up controller connections made.

--- PinBoard/SignUpViewController.swift
+++ PinBoard/SignUpViewController.swift
@@ -10,7 +10,12 @@ import UIKit
 import FirebaseAuth
 
 class SignUpViewController: UIViewController {
-
+    @IBOutlet weak var emailAddress: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
+    @IBAction func signUpButtonTouchedUp(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
