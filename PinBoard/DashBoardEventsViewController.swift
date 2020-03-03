c2bfeee1f7c52dc4adef9f38bd2576bdc5f60305
Alexis <s026134@students.lmsd.org>
2020-03-03 09:03:41 -0500

    Firebase Data base in posting and dashboard events view controller and add view controller

--- PinBoard/DashBoardEventsViewController.swift
+++ PinBoard/DashBoardEventsViewController.swift
@@ -7,8 +7,13 @@
 //
 
 import UIKit
+import FirebaseAuth
+import FirebaseDatabase
+import Firebase
 
 class DashBoardEventsViewController: UIViewController {
+//    UITableViewDelegate, UITableViewDataSource
+
     @IBOutlet weak var mainTableView: UITableView!
     
     @IBOutlet weak var mainCell: UITableViewCell!
@@ -16,11 +21,54 @@ class DashBoardEventsViewController: UIViewController {
     @IBOutlet weak var eventTableView: UITableView!
     @IBOutlet weak var eventCell: UITableViewCell!
     
+    var rootRef : DatabaseReference!
+    var refEvent : DatabaseReference!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        refEvent = Database.database().reference().child("Events")
+        
+        //retrieving and listening for data from firebase database
+        
+       guard let uid = Auth.auth().currentUser?.uid else{return}
+        refEvent.observe(.value, with: {(snap: DataSnapshot) in
+            if let actualEvent = snap.value {
+                if let date = actualEvent[]{
+                    self.mainCell.textLabel?.text = date
+                    self.mainTableView.reloadData()
+                }
+            }
+            
+        })
+//            //take value from snapshot and add to table view and its cell
+//            let event = snapshot.value as Any?
+//
+//            if let actualEvent = event {
+//                if let date = actualEvent ["Date"] {
+//                    self.mainCell.textLabel?.text = date
+//
+//                    self.mainTableView.reloadData()
+//                }
+//            }
+           
+           
+            
+            
 
         // Do any additional setup after loading the view.
     }
+    
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        <#code#>
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        <#code#>
+//    }
+    
+    
+    
     
 
     /*
