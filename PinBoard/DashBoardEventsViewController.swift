b55847aceefdff89038ef45e98e9bc51389b0869
Alexis <s026134@students.lmsd.org>
2020-03-07 22:21:59 -0500

    adjustments to main story board, create accounts with names and their profile pic, accessing firebase database :)

--- PinBoard/DashBoardEventsViewController.swift
+++ PinBoard/DashBoardEventsViewController.swift
@@ -12,8 +12,8 @@ import FirebaseDatabase
 import Firebase
 
 class DashBoardEventsViewController: UIViewController {
-//    UITableViewDelegate, UITableViewDataSource
-
+    //    UITableViewDelegate, UITableViewDataSource
+    
     @IBOutlet weak var mainTableView: UITableView!
     
     @IBOutlet weak var mainCell: UITableViewCell!
@@ -31,8 +31,8 @@ class DashBoardEventsViewController: UIViewController {
         refEvent = Database.database().reference().child("Events")
         
         //retrieving and listening for data from firebase database
-     /*
-       guard let uid = Auth.auth().currentUser?.uid else{return}
+        
+        guard let uid = Auth.auth().currentUser?.uid else{return}
         refEvent.observe(.value, with: {(snap: DataSnapshot) in
             if let actualEvent = snap.value {
                 if let date = actualEvent[]{
@@ -41,48 +41,47 @@ class DashBoardEventsViewController: UIViewController {
                 }
             }
             
-       
+            
             
         })
-         */
-//            //take value from snapshot and add to table view and its cell
-//            let event = snapshot.value as Any?
-//
-//            if let actualEvent = event {
-//                if let date = actualEvent ["Date"] {
-//                    self.mainCell.textLabel?.text = date
-//
-//                    self.mainTableView.reloadData()
-//                }
-//            }
-           
-           
-            
-            
-
+        //            //take value from snapshot and add to table view and its cell
+        //            let event = snapshot.value as Any?
+        //
+        //            if let actualEvent = event {
+        //                if let date = actualEvent ["Date"] {
+        //                    self.mainCell.textLabel?.text = date
+        //
+        //                    self.mainTableView.reloadData()
+        //                }
+        //            }
+        
+        
+        
+        
+        
         // Do any additional setup after loading the view.
     }
     
-//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        <#code#>
-//    }
-//
-//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        <#code#>
-//    }
+    //    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    //        <#code#>
+    //    }
+    //
+    //    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    //        <#code#>
+    //    }
     
     
     
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
