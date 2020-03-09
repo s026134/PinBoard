8938c67c55dfb32e74e5451bd4e5d1efe0c2414b
Justin Minerva <s013836@students.lmsd.org>
2020-03-09 09:30:23 -0400

    Preference View controller

---
+++ PinBoard/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  PinBoard
+//
+//  Created by Justin Minerva (student LM) on 3/9/20.
+//  Copyright © 2020 Alexis Duong (student LM). All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var label: UILabel!
+}
--- PinBoard/PreferencesViewController.swift
+++ PinBoard/PreferencesViewController.swift
@@ -9,12 +9,37 @@
 import UIKit
 import FirebaseAuth
 
-class PreferencesViewController: UIViewController {
+class PreferencesViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    var collectionData = ["🎮 Gaming", "🎶 Music", "➕Math", "🔬 Science", "🥏 Sports ", "📚 Reading", "💻 Computer Science", " 📺 TV", "🌮 Food", "🤪 Misc"]
+       
+       var userTags: Set<Int> = []
 
-    }
+       override func viewDidLoad() {
+           super.viewDidLoad()
+       }
+       
+       override func didReceiveMemoryWarning() {
+           super.didReceiveMemoryWarning()
+       }
+       
+       func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
+           return collectionData.count
+       }
+       
+       func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath)->UICollectionViewCell{
+           let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CollectionViewCell
+           cell.label.text = collectionData[indexPath.item]
+           cell.layer.cornerRadius = 35
+          
+           return cell
+       }
+       
+       func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath){
+           userTags.insert(indexPath.item)
+           print(userTags)
+       }
+
     
 
 }
