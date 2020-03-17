4207bf2f7e915327149cffc6373acbe470366d3d
Justin Minerva <s013836@students.lmsd.org>
2020-03-16 22:45:34 -0400

    Preference View Controller updated for selecting and deselecting cells.

--- PinBoard/PreferencesViewController.swift
+++ PinBoard/PreferencesViewController.swift
@@ -10,36 +10,42 @@ import UIKit
 import FirebaseAuth
 
 class PreferencesViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
-
-    var collectionData = ["🎮 Gaming", "🎶 Music", "➕Math", "🔬 Science", "🥏 Sports ", "📚 Reading", "💻 Computer Science", " 📺 TV", "🌮 Food", "🤪 Misc"]
-       
-       var userTags: Set<Int> = []
-
-       override func viewDidLoad() {
-           super.viewDidLoad()
-       }
-       
-       override func didReceiveMemoryWarning() {
-           super.didReceiveMemoryWarning()
-       }
-       
-       func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
-           return collectionData.count
-       }
-       
-       func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath)->UICollectionViewCell{
-           let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CollectionViewCell
-           cell.label.text = collectionData[indexPath.item]
-           cell.layer.cornerRadius = 35
-          
-           return cell
-       }
-       
-       func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath){
-           userTags.insert(indexPath.item)
-           print(userTags)
-       }
-
     
-
+    var collectionData = ["🎮 Gaming", "🎶 Music", "➕Math", "🔬 Science", "🥏 Sports ", "📚 Reading", "💻 Computer Science", " 📺 TV", "🌮 Food", "🤪 Misc"]
+    
+    var userTags: Set<Int> = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int{
+        return collectionData.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath)->UICollectionViewCell{
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CollectionViewCell
+        cell.label.text = collectionData[indexPath.item]
+        cell.layer.cornerRadius = 35
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath){
+        let cell = collectionView.cellForItem(at: indexPath)
+        cell?.contentView.backgroundColor = #colorLiteral(red: 1, green: 0.8323456645, blue: 0.4732058644, alpha: 1)
+        if let index = userTags.firstIndex(of: indexPath.item){
+            userTags.remove(at: index)
+            cell?.contentView.backgroundColor = #colorLiteral(red: 0.5787474513, green: 0.3215198815, blue: 0, alpha: 1)
+        }
+        else{
+            userTags.insert(indexPath.item)
+        }
+        print(userTags)
+    }
+    
 }
