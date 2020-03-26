ee2d451a7a8cd066b4ff3ab950d615b39079fbcf
Claire Lu <s013384@students.lmsd.org>
2020-03-25 20:11:55 -0400

    changed tab images to be smaller

--- PinBoard/AppDelegate.swift
+++ PinBoard/AppDelegate.swift
@@ -15,7 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         FirebaseApp.configure()
-        
+        UITabBar.appearance().barTintColor = .black
+        UITabBar.appearance().tintColor = .systemPink
 //        let authListener = Auth.auth().addStateDidChangeListener({auth, user in
 //            
 //            if user != nil{
