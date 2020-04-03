181142a22191c74f563f6b31f5d80d5682104544
Claire Lu <s013384@students.lmsd.org>
2020-04-03 19:18:40 -0400

    created a channels view controller

---
+++ PinBoard/ChannelsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ChannelsViewController.swift
+//  PinBoard
+//
+//  Created by Claire Lu (student LM) on 4/3/20.
+//  Copyright © 2020 Alexis Duong (student LM). All rights reserved.
+//
+
+import UIKit
+
+class ChannelsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
